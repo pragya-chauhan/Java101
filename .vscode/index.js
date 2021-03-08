abbac269e557fe71eed880bc77d6630a0f5f529a
pragya-chauhan <pragyasinghchauhan13@gmail.com>
2021-03-08 09:28:22 -0500

    basic json program

--- .vscode/index.js
+++ .vscode/index.js
@@ -3,11 +3,12 @@ const moment = require('moment');
 const simpleGit = require('simple-git')
 const FILE_PATH = './data.json';
 
-const DATE = moment().format();
+const DATE = moment().subtract(1,'d').format();
 
 const data = {
     date:DATE
 }
-jsonfile.writeFile(FILE_PATH,data);
+jsonfile.writeFile(FILE_PATH,data,()=>{
+    simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
+});
 //git commit --date = "Mon....."
-simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE}).push();
